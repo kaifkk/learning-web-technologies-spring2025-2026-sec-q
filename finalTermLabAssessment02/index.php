795b54a070e92fe87aa32213522326149419c31a
kaifkk <https://kaifkk@github.com>
2026-04-15 13:23:38 +0600

    building home page

---
+++ finalTermLabAssessment02/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>labTask</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <div class="navBarParent">
+            <div>
+                <h1>X Company</h1>
+            </div>
+            <div>
+                <a href="index.php">Home</a>
+                <a href="#">Login</a>
+                <a href="#">Registration</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
