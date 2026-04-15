df79e0ac5e61d54564c415126e74aeb3e73bab77
kaifkk <https://kaifkk@github.com>
2026-04-15 14:55:18 +0600

    display username in dashboard

--- finalTermLabAssessment02/dashboard.php
+++ finalTermLabAssessment02/dashboard.php
@@ -4,8 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>hello</h1>
+        <div class="container">
+        <div class="navBarParent">
+            <div>
+                <h1>X Company</h1>
+            </div>
+            <div class="links">
+                <a href="index.php">Home</a>
+                <a href="login.php">Login</a>
+                <a href="registration.php">Registration</a>
+            </div>
+        </div>
+
+        <div>
+            <?php
+                session_start();
+                if (!isset($_SESSION["current_user"])) {
+                    header("Location: login.php");
+                    exit();
+                }
+
+                $username = $_SESSION['current_user'];
+                echo "welcome: " . $username;
+            ?>
+        </div>
+
+        <footer>
+            <p>Copyright © 2017</p>
+        </footer>
+    </div>
 </body>
 </html>
